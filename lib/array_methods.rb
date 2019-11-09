1e0f8eb59790e64f76518014d56393cd885ce240
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-09 00:48:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  p array.index(value_to_find)
 end
 
 def find_max_value(array)
-  array.max
+  p array.max
 end
 
 def find_min_value(array)
-  array.min
+  p array.min
 end
